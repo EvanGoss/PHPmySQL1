631cc1d6ce0631d0e6ad6f46a9ffdc958927c88f
Evan Goss <evan@evangoss.com>
2017-02-13 23:39:48 -0800

    Initial commit. Display prices.

---
+++ prices.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Exotics and Pets - Prices</title>
+</head>
+<body>
+  <?php
+    $prices = array('Fish Food' => 3);
+    $prices['Cat Food'] = 8;
+    $prices['Bird Food'] = 2;
+    $prices['Dog Food'] = 6;
+
+    while ($element = each($prices)) {
+      echo $element['key'];
+      echo " - ";
+      echo $element['value'];
+      echo "<br />";
+    }
+  ?>
+</body>
+</html>
