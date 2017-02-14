da371b032d735924ccdbeddb92c924febb3705ae
Evan Goss <evan@evangoss.com>
2017-02-13 23:25:25 -0800

    Initial commit. Display array items.

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>array.php</title>
+</head>
+<body>
+  <?php
+    $products = array('Dog Food',
+                      'Cat Food',
+                      'Fish Food',
+                      'Bird Food');
+
+    for ($item = 0; $item < count($products); $item++) {
+      echo '|'.$products[$item].'|<br />';
+    }
+  ?>
+</body>
+</html>
