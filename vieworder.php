542abda8d473963a88cb483a167c22d0c2d8d7a3
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-07 12:52:08 -0800

    Display orders in table

--- vieworder.php
+++ vieworder.php
@@ -3,28 +3,69 @@
 <head>
   <meta charset="UTF-8">
   <title>Exotics and Pets - Order Results</title>
+  
+  <style type="text/css">
+    table, th, td {
+      border-collapse: collapse;
+      border: 1px solid black;
+      padding: 6px;
+    }
+
+    th {
+      background: #ccccff;
+    }
+  </style>
+
 </head>
+
 <body>
   <h1>Exotics and Pets</h1>
-  <h2>Order Results</h2>
+  <h2>Customer Orders</h2>
   <?php
     $document_root = $_SERVER['DOCUMENT_ROOT'];
-    @$fp = fopen($document_root."/cim/orders/orders.txt", 'rb');
-    flock($fp, LOCK_SH); // lock file for reading
+    @$orders = file($document_root."/cim/orders/orders.txt");
 
-    if (!$fp) {
+    $number_of_orders = count($orders);
+
+    if ($number_of_orders == 0) {
       echo "<p><strong>No orders pending.<br />
         Please try again later.</strong></p>";
       exit;
     }
 
-    while (!feof($fp)) {
-      $order = fgets($fp);
-      echo htmlspecialchars($order)."<br />";
-    }
+    echo "<table>\n";
+    echo "<tr>
+      <th>Order Date</th>
+      <th>Bird Food</th>
+      <th>Cat Food</th>
+      <th>Dog Food</th>
+      <th>Fish Food</th>
+      <th>Total</th>
+      <th>Address</th>
+      </tr>";
 
-    flock($fp, LOCK_UN); // release read lock
-    fclose($fp);
+    for ($i = 0; $i < $number_of_orders; $i++) {
+      // split up each line
+      $line = explode("\t", $orders[$i]);
+
+      // keep only the number of items ordered
+      $line[1] = intval($line[1]);
+      $line[2] = intval($line[2]);
+      $line[3] = intval($line[3]);
+      $line[4] = intval($line[4]);
+
+      // output each order
+      echo "<tr>
+        <td>".$line[0]."</td>
+        <td style=\"text-align: right;\">".$line[1]."</td>
+        <td style=\"text-align: right;\">".$line[2]."</td>
+        <td style=\"text-align: right;\">".$line[3]."</td>
+        <td style=\"text-align: right;\">".$line[4]."</td>
+        <td style=\"text-align: right;\">".$line[5]."</td>
+        <td>".$line[6]."</td>
+        </tr>";
+    }
+    echo "</table>";
   ?>
 </body>
 </html>
