103dc450eb51119521bfbe8e7bc10482dd0b3ca0
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-07 10:53:13 -0800

    Initial commit. Display products on page.

---
+++ products.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Exotics and Pets - Products</title>
+</head>
+<body>
+  <?php
+    $products = array(
+                  array('Code' => 'BF',
+                        'Description' => 'Bird Food',
+                        'Price' => 2),
+                  array('Code' => 'CF',
+                        'Description' => 'Cat Food',
+                        'Price' => 8),
+                  array('Code' => 'DF',
+                        'Description' => 'Dog Food',
+                        'Price' => 6),
+                  array('Code' => 'FF',
+                        'Description' => 'Fish Food',
+                        'Price' => 3)
+                );
+
+    for ($row = 0; $row < count($products); $row++) {
+      echo '|'.$products[$row]['Code'].'|'.
+               $products[$row]['Description'].'|'.
+               $products[$row]['Price'].'|<br />';
+    }
+  ?>
+</body>
+</html>
