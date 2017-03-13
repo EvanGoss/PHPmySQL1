6530ffabf6b1302d8bb0ebd0dd521638841a27c5
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-13 01:33:18 -0700

    Demonstrate recursion

---
+++ recursion.php
@@ -0,0 +1,21 @@
+<?php
+  function reverse_r($str) {
+    if (strlen($str)) {
+      reverse_r(substr($str, 1));
+    }
+    echo substr($str, 0, 1);
+    return;
+  }
+
+  function reverse_i($str) {
+    for ($i = 1; $i <= strlen($str); $i++) {
+      echo substr($str, -$i, 1);
+    }
+    return;
+  }
+
+  echo 'Recursive<br />';
+  reverse_r('Hello');
+  echo '<br /><br />Iterative<br />';
+  reverse_i('Hello');
+?>
