252c4b4828bcc60109533f48226906e40cc4a2b9
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-13 01:22:19 -0700

    Refactor

--- prices.php
+++ prices.php
@@ -1,21 +1,71 @@
+<?php
+  $prices = array('Fish Food' => 3);
+  $prices['Cat Food'] = 8;
+  $prices['Bird Food'] = 2;
+  $prices['Dog Food'] = 6;
+
+  function display_prices($prices) {
+    echo "<table>\n";
+    echo "<tr>
+      <th>Product</th>
+      <th>Price</th>
+      </tr>";
+    while (list($product, $price) = each($prices)) {
+      // output each price
+      echo "<tr>
+        <td>".$product."</td>
+        <td>".format_price($price)."</td>
+        </tr>";
+    }
+    echo "</table>";
+  }
+
+  function format_price($price) {
+    $formattedprice = sprintf("%.2f", $price);
+    return $formattedprice;
+  }
+?>
+  
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>Exotics and Pets - Prices</title>
+  <link rel="stylesheet" href="styles/styles.css" type="text/css">
+
+  <style type="text/css">
+    table, th, td {
+      border-collapse: collapse;
+      border: 1px solid black;
+      padding: 6px;
+    }
+
+    th {
+      background: #ccccff;
+    }
+  </style>
 </head>
 <body>
-  <?php
-    $prices = array('Fish Food' => 3);
-    $prices['Cat Food'] = 8;
-    $prices['Bird Food'] = 2;
-    $prices['Dog Food'] = 6;
+  <header>
+    <?php require('header.php'); ?>
+  </header>
 
-    ksort($prices);
+  <!-- nav menu -->
+  <nav id="menu">
+    <?php require('menu.php'); ?>
+  </nav>
 
-    while (list($product, $price) = each($prices)) {
-      echo $product." - ".$price."<br />";
-    }
+  <br /><br />
+  <h2>Prices</h2>
+
+  <?php 
+    display_prices($prices);
   ?>
+
+  <!-- page footer -->
+  <footer>
+    <?php require('footer.php'); ?>
+  </footer>
+    
 </body>
 </html>
